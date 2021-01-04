2f158de77ef53e2ae5fcb17a56eddb27448be34e
Sam Tovey <samtovey13@gmail.com>
2021-01-04 16:43:48 +0000

    added json file with mock data, imported this to App via a LocationDetails component. All working and displaying City, Country in the browser. Replaced favicon with weather icon.

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import '../App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import LocationDetails from './location-details';
+
+const App = props => {
+  return <LocationDetails 
+    city={props.location.city} 
+    country={props.location.country}
+  />;
+};
+
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import render from 'react-dom';
+import App from './components/App';
+import { location } from './data/forecast.json';
+// import './index.css';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  <App location={location}/>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
